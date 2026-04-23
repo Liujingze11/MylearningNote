b71b52b6ee216a76dd3d66847a2861342cc1d923
liujingze <18741009909@163.com>
2026-04-23 15:51:06 +0800

    改为ipynb格式

--- Leetcode_Python/001/way1.py
+++
@@ -1,6 +0,0 @@
-class Solution(object):
-    def twoSum(self,nums,target):
-        for i in range(lens(nums)):
-            for j in range(i+1,lens(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
